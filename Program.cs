f42e78e7bde107757d2d9bf02ea3eb193a8b7528
Mikhail Pilin <mikhail@pilin.name>
2022-02-22 20:18:11 +0300

    Support cleanup for SOFTWARE\Classes\Installer

--- Program.cs
+++ Program.cs
@@ -20,21 +20,100 @@ namespace EtwHostServiceMsiCleanUp
       return builder.ToString();
     }
 
-    private static void CleanUpIf(RegistryKey key, Func<RegistryKey, string, bool> func)
+    private static T NotNull<T>(this T t) where T : class
+    {
+      if (t == null)
+        throw new NullReferenceException();
+      return t;
+    }
+
+    private static string GetPad(int pad) => new(' ', 2 * pad);
+
+    private static void CleanUpIf(int pad, RegistryKey key, Func<RegistryKey, string, bool> func)
     {
       if (key == null) throw new ArgumentNullException(nameof(key));
       if (func == null) throw new ArgumentNullException(nameof(func));
       foreach (var subKeyName in key.GetSubKeyNames())
       {
         bool toDelete;
-        using (var subKey = key.OpenSubKey(subKeyName))
+        using (var subKey = key.OpenSubKey(subKeyName).NotNull())
           toDelete = func(subKey, subKeyName);
         if (toDelete)
         {
-          Console.WriteLine("  {0}", subKeyName);
+          Console.WriteLine("{0}{1}", GetPad(pad), subKeyName);
           key.DeleteSubKeyTree(subKeyName);
         }
       }
+    }
+
+    private static void CleanUpUpgradeCodes(int pad, RegistryKey key)
+    {
+      if (key == null) throw new ArgumentNullException(nameof(key));
+
+      var masterUpgradeCode = new Guid("25CB994F-CDCF-421B-9156-76528AAFC0E1").ToMsiPresentation();
+      if (masterUpgradeCode != "F499BC52FCDCB12419656725A8FA0C1E")
+        throw new Exception("Failed");
+
+      using var upgradeCodesKey = key.OpenSubKey(@"UpgradeCodes", true);
+      if (upgradeCodesKey == null)
+        return;
+
+      Console.WriteLine("{0}UpgradeCodes:", GetPad(pad));
+      CleanUpIf(pad + 1, upgradeCodesKey, (_, subKeyName) => subKeyName == masterUpgradeCode);
+    }
+
+    private static void CleanUpComponents(int pad, RegistryKey key)
+    {
+      if (key == null) throw new ArgumentNullException(nameof(key));
+
+      var serviceOldComponent = new Guid("1D4FAF23-64A9-4B77-AACE-1AB92385D09C").ToMsiPresentation();
+      if (serviceOldComponent != "32FAF4D19A4677B4AAECA19B32580DC9")
+        throw new Exception("Failed");
+
+      using var componentsKey = key.OpenSubKey("Components", true);
+      if (componentsKey == null)
+        return;
+
+      Console.WriteLine("{0}Components:", GetPad(pad));
+      CleanUpIf(pad + 1, componentsKey, (subKey, subKeyName) =>
+        {
+          if (subKeyName == serviceOldComponent)
+            return true;
+
+          foreach (var valueName in subKey.GetValueNames())
+            if (subKey.GetValue(valueName) is string str && str.Contains("ETW Host"))
+              return true;
+          return false;
+        });
+    }
+
+    private static void CleanUpProducts(int pad, RegistryKey key)
+    {
+      if (key == null) throw new ArgumentNullException(nameof(key));
+
+      using var productsKey = key.OpenSubKey("Products", true);
+      if (productsKey == null)
+        return;
+
+      Console.WriteLine("{0}Products:", GetPad(pad));
+      CleanUpIf(pad + 1, productsKey, (subKey, _) =>
+        {
+          using var propertySubKey = subKey.OpenSubKey("InstallProperties");
+          return propertySubKey?.GetValue("DisplayName") is string str && (str.StartsWith("JetBrains ETW Host Service") || str.Equals("JetBrains ETW Service"));
+        });
+    }
+
+    private static void CleanUpClassProducts(int pad, RegistryKey key)
+    {
+      if (key == null)
+        throw new ArgumentNullException(nameof(key));
+
+      using var productsKey = key.OpenSubKey("Products", true);
+      if (productsKey == null)
+        return;
+
+      Console.WriteLine("{0}ClassProducts:", GetPad(pad));
+      CleanUpIf(pad + 1, productsKey, (subKey, _) => subKey?.GetValue("ProductName") is string str && (str.StartsWith("JetBrains ETW Host Service") || str.Equals("JetBrains ETW Service")));
     }
 
     private static int Main()
@@ -44,51 +123,29 @@ namespace EtwHostServiceMsiCleanUp
         var version = ((AssemblyInformationalVersionAttribute)typeof(Program).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false).First()).InformationalVersion;
         Console.WriteLine("ETW Host Service MSI CleanUp Tool v{0} Mikhail Pilin", version);
 
-        var masterUpgradeCode = new Guid("25CB994F-CDCF-421B-9156-76528AAFC0E1").ToMsiPresentation();
-        var serviceOldComponent = new Guid("1D4FAF23-64A9-4B77-AACE-1AB92385D09C").ToMsiPresentation();
-
-        if (masterUpgradeCode != "F499BC52FCDCB12419656725A8FA0C1E")
-          throw new Exception("Failed");
-        if (serviceOldComponent != "32FAF4D19A4677B4AAECA19B32580DC9")
-          throw new Exception("Failed");
-
         if (!new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator))
           throw new Exception("Run under the elevated administrator");
 
-        using var installerKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Installer");
+        Console.WriteLine(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Installer:");
+        using (var installerKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Installer").NotNull())
+        {
+          CleanUpUpgradeCodes(1, installerKey);
+          using (var userSidsKey = installerKey.OpenSubKey("UserData").NotNull())
+            foreach (var userSidName in userSidsKey.GetSubKeyNames())
+            {
+              Console.WriteLine("{0}{1}:", GetPad(1), userSidName);
+              using var userSidKey = userSidsKey.OpenSubKey(userSidName).NotNull();
+              CleanUpComponents(2, userSidKey);
+              CleanUpProducts(2, userSidKey);
+            }
+        }
 
-        Console.WriteLine("UpgradeCodes:");
-        using (var upgradeCodesKey = installerKey!.OpenSubKey(@"UpgradeCodes", true))
-          CleanUpIf(upgradeCodesKey, (_, subKeyName) => subKeyName == masterUpgradeCode);
-
-        using (var userSidsKey = installerKey!.OpenSubKey("UserData"))
-          foreach (var userSidName in userSidsKey!.GetSubKeyNames())
-          {
-            using var userSidKey = userSidsKey!.OpenSubKey(userSidName);
-
-            Console.WriteLine("Components {0}:", userSidName);
-            using (var componentsKey = userSidKey!.OpenSubKey("Components", true))
-              if (componentsKey != null)
-                CleanUpIf(componentsKey, (subKey, subKeyName) =>
-                  {
-                    if (subKeyName == serviceOldComponent)
-                      return true;
-
-                    foreach (var valueName in subKey!.GetValueNames())
-                      if (subKey!.GetValue(valueName) is string str && str.Contains("ETW Host"))
-                        return true;
-                    return false;
-                  });
-
-            Console.WriteLine("Products {0}:", userSidName);
-            using (var productsKey = userSidKey!.OpenSubKey("Products", true))
-              if (productsKey != null)
-                CleanUpIf(productsKey, (subKey, _) =>
-                  {
-                    using var propertySubKey = subKey.OpenSubKey("InstallProperties");
-                    return propertySubKey?.GetValue("DisplayName") is string str && (str.StartsWith("JetBrains ETW Host Service") || str.Equals("JetBrains ETW Service"));
-                  });
-          }
+        Console.WriteLine(@"SOFTWARE\Classes\Installer:");
+        using (var installerClassesKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Classes\Installer").NotNull())
+        {
+          CleanUpUpgradeCodes(1, installerClassesKey);
+          CleanUpClassProducts(1, installerClassesKey);
+        }
 
         Console.WriteLine("Done");
         return 0;
